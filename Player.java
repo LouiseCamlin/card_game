72c77bf1c1beac5e57fc4dc373c9f32552b68474
Louise Camlin <lacamlin@gmail.com>
2017-05-19 17:14:19 +0100

    first commit

---
+++ Player.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Player {
+
+  private String name;
+  private ArrayList<Card> hand;
+
+  public Player (String name) {
+    this.hand = new ArrayList<Card>();
+    this.name = name;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public int handSizeCount() {
+    return this.hand.size();
+  }
+
+  public void addCardToHand(Card card) {
+    this.hand.add(card);
+  }
+
+
+
+}
---
+++ PlayerTest.java
@@ -0,0 +1,30 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.*;
+
+
+public class PlayerTest {
+  Player player;
+  Card card;
+
+
+  @Before
+  public void before() {
+    player = new Player("Irma");
+  }
+
+  @Test
+  public void playerHasName() {
+    assertEquals("Irma", player.getName());
+  }
+
+  @Test
+  public void handStartsEmpty() {
+    assertEquals(0, player.handSizeCount());
+  }
+
+  @Test
+  public void playerCanPickUpCard() {
+    player.addCardToHand(card);
+    assertEquals(1, player.handSizeCount());
+  }
+}
